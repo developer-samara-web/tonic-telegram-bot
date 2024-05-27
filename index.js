67be39f4138b9f14bcaf0410adc8e0ae08d00e21
a1kashnya <k.popkoff@yandex.ru>
2024-05-27 22:34:15 +0400

    Add hears & stages to index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-//index.js
+//? INDEX.JS
 
+// Global
 require('dotenv').config();
 require('module-alias/register');
 
 // Requires
 const { Bot } = require('@config/telegram')
+const { Scenes, session } = require('telegraf')
+
+// Require
+const stages = require('@config/stages')
+const hears = require('@config/hears')
+
+// Stages
+const stage = new Scenes.Stage(stages)
+
+// Session & Middleware & Hears
+Bot.use(session())
+Bot.use(stage.middleware())
+hears(Bot)
 
 // Launch
 Bot.launch()
